a40a8f3473de99fcf9c01599ca595ea3d843d5a6
timotheekurniawan <timothee@connect.hku.hk>
2021-10-09 01:45:59 +0800

    Search bar on Products

--- react-frontend/src/components/DashboardSidebar.js
+++ react-frontend/src/components/DashboardSidebar.js
@@ -44,11 +44,6 @@ const items = [
     title : "My Kanban"
   },
   {
-    href: '/app/customers',
-    icon: UsersIcon,
-    title: 'Customers'
-  },
-  {
     href: '/app/network',
     icon: UsersIcon,
     title: 'My Network'
@@ -63,11 +58,6 @@ const items = [
     icon: InboxIcon,
     title: 'Inbox',
   },
-  // {
-  //   href: '/app/account',
-  //   icon: UserIcon,
-  //   title: 'Account'
-  // },
   {
     href: '/app/settings',
     icon: SettingsIcon,
