3e9c3a49955b511c7c7461cca99f2918a8dbebae
MINSEOK OH <oms0803@gmail.com>
2021-10-09 08:57:12 +0800

    Update sample messages

--- react-frontend/src/__mocks__/messages.js
+++ react-frontend/src/__mocks__/messages.js
@@ -3,50 +3,50 @@ import { v4 as uuid } from 'uuid';
 export default [
   {
     id: uuid(),
-    receivedAt: '27/03/2019',
-    body: 'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
-    title: 'Dropbox',
+    receivedAt: '09/10/2021',
+    body: 'Good work on increasing your Total Sales! Since your number of farm plans is getting weak, let me introduce you to my partner.',
+    title: 'Regarding the crop yield',
     senderType: 'Investor',
-    senderName: 'Minseok Oh'
+    senderName: 'Jamie Dimon'
   },
   {
     id: uuid(),
-    receivedAt: '27/03/2019',
-    body: 'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
-    title: 'Dropbox',
+    receivedAt: '27/09/2021',
+    body: 'We have not really met each other since the last project on calamansi farms. Lets catch up real soon',
+    title: 'Lets Catch UP!!!!',
+    senderType: 'Partner RPO',
+    senderName: 'Akash Vinson'
+  },
+  {
+    id: uuid(),
+    receivedAt: '21/05/2021',
+    body: 'The new platform for your RPO looks super cool!! Did you update the website? Must be really glad about it',
+    title: 'Updated your website?',
     senderType: 'Partner',
-    senderName: 'Minseok Oh'
+    senderName: 'Minseok'
   },
   {
     id: uuid(),
-    receivedAt: '27/03/2019',
-    body: 'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
-    title: 'Dropbox',
-    senderType: 'Partner',
-    senderName: 'Minseok Oh'
-  },
-  {
-    id: uuid(),
-    receivedAt: '27/03/2019',
-    body: 'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
-    title: 'Dropbox',
+    receivedAt: '25/12/2020',
+    body: 'While searching through the list of RPOs, your RPO had caught my attention. Should we talk more in Zoom?',
+    title: 'Tell me about your products in detail',
     senderType: 'Investor',
-    senderName: 'Minseok Oh'
+    senderName: 'Cillian Odling'
   },
   {
     id: uuid(),
-    receivedAt: '27/03/2019',
-    body: 'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
-    title: 'Dropbox',
+    receivedAt: '17/07/2020',
+    body: 'Is your farm okay from the heavy rain? Please let me know if you need any assistance.',
+    title: 'HEY U OKAY',
     senderType: 'Investor',
-    senderName: 'Minseok Oh'
+    senderName: 'Vu Minh Dang '
   },
   {
     id: uuid(),
-    receivedAt: '27/03/2019',
-    body: 'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
-    title: 'Dropbox',
-    senderType: 'Investor',
-    senderName: 'Minseok Oh'
+    receivedAt: '24/03/2010',
+    body: 'Here is the copy of our Family Farmers Profiling Survey (responded to by the RPO members) RPO Profile, RPO Growth Monitoring',
+    title: 'Code for Good',
+    senderType: 'Asia Dhrra',
+    senderName: 'Arcee Pila'
   }
 ];
