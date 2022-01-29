de4aa1cb55b7a768b29dabf1e2dc97277847ff76
Anuj Bhatia <anuj@connect.hku.hk>
2022-01-29 21:28:18 +0800

    Image for screencap

--- react-frontend/src/pages/Kanban.js
+++ react-frontend/src/pages/Kanban.js
@@ -1,92 +1,11 @@
-import Board from 'react-trello'
-import './Kanban.css'
-const Kanban = () => {
-    const categories = {
-        lanes: [
-          {
-            id: 'year',
-            title: 'Yearly Goals and Tasks',
-            cards: [
-              {
-                description: 'Construction of new processing facility',
-                id: 'processing facility',
-                label: 'Urgent',
-                laneId: 'PLANNED',
-                title: 'Processing Facility'
-              },
-            ],
-            style: {backgroundColor: 'white'},
-            cardStyle : {
-              backgroundColor: '#FFDDE2FF'
-            }
-          },
-          {
-            id: 'month',
-            title: 'Monthly Goals and Tasks',
-            cards: [
-              {
-                description: 'Development of Calamansi Learning Site with IDOFS',
-                id: 'learning-site',
-                label: 'Medium',
-                laneId: 'PLANNED',
-                title: 'Learning Site'
-              },
-              {
-                description: 'Renovation of existing office/meeting hall',
-                id: 'renovation',
-                label: 'low',
-                laneId: 'PLANNED',
-                title: 'Office Renovation'
-              },
-            ],
-            style: {backgroundColor: 'white'},
-            cardStyle : {backgroundColor: '#FAA094FF'}
+const Kanban = () => (
+  <div style={{ backgroundColor: 'white', height: '100vh' }}>
+    <img
+      alt="Logo"
+      src="/static/screenapi.jpg"
+    />
+  </div>
 
-          },
-          {
-            id: 'week',
-            title: 'Weekly Goals and Tasks',
-            cards: [
-              {
-                description: 'Calamansi Production Technology Trainings',
-                id: 'Calamansi-tech',
-                label: 'Urgent',
-                laneId: 'PLANNED',
-                title: 'Technology Trainings'
-              },
-            ],
-            style: {backgroundColor: 'white'},
-            cardStyle : {backgroundColor: '#9ED9CCFF'}
+);
 
-          },
-          {
-            id: 'completed',
-            title: 'Completed Tasks',
-            cards: [
-              {
-                description: 'Construction of water refilling station',
-                id: 'water-station',
-                label: 'Medium',
-                laneId: 'PLANNED',
-                title: 'Water Station'
-              },
-            ],
-            style: {backgroundColor: 'white'},
-            cardStyle : {backgroundColor: '#008C76FF'}
-
-          }
-        ]
-    }
-    return (
-        <>
-            <h1>My Kanban Board</h1>
-            <Board 
-                style = {{backgroundColor : '#f4f6f8'}}
-                cardStyle = {{backgroundColor: '#eec'}}
-                data={categories} editable="True"/>
-        </>
-
-    )
-}
-
-export default Kanban
+export default Kanban;
--- react-frontend/src/pages/TextConverter.js
+++ react-frontend/src/pages/TextConverter.js
@@ -3,7 +3,7 @@ import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import * as Yup from 'yup';
 import { Formik } from 'formik';
-import './TextConverter.css'
+import './TextConverter.css';
 import {
   Box,
   Button,
@@ -32,36 +32,36 @@ import { FileUploader } from 'react-drag-drop-files';
 import BasicModal from '../components/Modal';
 import Spinner from '../components/Spinner';
 
-const fileTypes = ["JSON", "CSV"];
+const fileTypes = ['JSON', 'CSV'];
 
 const tableData = {
-  "12 months ended": ["Sep 25, 2021", "Sep 26, 2020", "Sep 28, 2019", "Sep 29, 2018", "Sep 30, 2017", "Sep 24, 2016"],
-  "Products": ["2,97,392", "2,20,747", "2,13,883", "2,25,847", "1,96,534", "0"],
-  "Services": ["68,425", "53,768", "46,291", "39,748", "32,700", "0"],
-  "Net sales(legacy)": ["0", "0", "0", "0", "0", "2,15,639"],
-  "Net sales": ["3,65,817", "2,74,515", "2,60,174", "2,65,595", "2,29,234", "2,15,639"],
-  "Products": ["-1,92,266", "-1,51,286", "-1,44,996", "-1,48,164", "-1,26,337", "0"],
-  "Services": ["-20,715", "-18,273", "-16,786", "-15,592", "-14,711", "0"],
-  "Cost of sales(legacy)": ["0", "0", "0", "0", "0", "-1,31,376"],
-  "Cost of sales": ["-2,12,981", "-1,69,559", "-1,61,782", "-1,63,756", "-1,41,048", "-1,31,376"],
-  "Gross margin": ["1,52,836", "1,04,956", "98,392", "1,01,839", "88,186", "84,263"],
-  "Research and development": ["-21,914", "-18,752", "-16,217", "-14,236", "-11,581", "-10,045"],
-  "Selling, general and administrative": ["-21,973", "-19,916", "-18,245", "-16,705", "-15,261", "-14,194"],
-  "Operating expenses": ["-43,887", "-38,668", "-34,462", "-30,941", "-26,842", "-24,239"],
-  "Operating income": ["1,08,949", "66,288", "63,930", "70,898", "61,344", "60,024"],
-  "Interest and dividend income": ["2,843", "3,763", "4,961", "5,686", "5,201", "3,999"],
-  "Interest expense": ["-2,645", "-2,873", "-3,576", "-3,240", "-2,323", "-1,456"],
-  "Other income (expense), net": ["60", '-87', "422", "-441", "-133", "-1,195"],
-  "Other income (expense), net": ["258", "803", "1,807", "2,005", "2,745", "1,348"],
-  "Income before provision for income taxes": ["1,09,207", "67,091", "65,737", "72,903", "64,089", "61,372"],
-  "Provision for income taxes": ["-14,527", "-9,680", "-10,481", "-13,372", "-15,738", "-15,685"],
-  "Net income": ["94,680", "57,411", "55,256", "59,531", "48,351", "45,687"]
-}
+  '12 months ended': ['Sep 25, 2021', 'Sep 26, 2020', 'Sep 28, 2019', 'Sep 29, 2018', 'Sep 30, 2017', 'Sep 24, 2016'],
+  Products: ['2,97,392', '2,20,747', '2,13,883', '2,25,847', '1,96,534', '0'],
+  Services: ['68,425', '53,768', '46,291', '39,748', '32,700', '0'],
+  'Net sales(legacy)': ['0', '0', '0', '0', '0', '2,15,639'],
+  'Net sales': ['3,65,817', '2,74,515', '2,60,174', '2,65,595', '2,29,234', '2,15,639'],
+  Products: ['-1,92,266', '-1,51,286', '-1,44,996', '-1,48,164', '-1,26,337', '0'],
+  Services: ['-20,715', '-18,273', '-16,786', '-15,592', '-14,711', '0'],
+  'Cost of sales(legacy)': ['0', '0', '0', '0', '0', '-1,31,376'],
+  'Cost of sales': ['-2,12,981', '-1,69,559', '-1,61,782', '-1,63,756', '-1,41,048', '-1,31,376'],
+  'Gross margin': ['1,52,836', '1,04,956', '98,392', '1,01,839', '88,186', '84,263'],
+  'Research and development': ['-21,914', '-18,752', '-16,217', '-14,236', '-11,581', '-10,045'],
+  'Selling, general and administrative': ['-21,973', '-19,916', '-18,245', '-16,705', '-15,261', '-14,194'],
+  'Operating expenses': ['-43,887', '-38,668', '-34,462', '-30,941', '-26,842', '-24,239'],
+  'Operating income': ['1,08,949', '66,288', '63,930', '70,898', '61,344', '60,024'],
+  'Interest and dividend income': ['2,843', '3,763', '4,961', '5,686', '5,201', '3,999'],
+  'Interest expense': ['-2,645', '-2,873', '-3,576', '-3,240', '-2,323', '-1,456'],
+  'Other income (expense), net': ['60', '-87', '422', '-441', '-133', '-1,195'],
+  'Other income (expense), net': ['258', '803', '1,807', '2,005', '2,745', '1,348'],
+  'Income before provision for income taxes': ['1,09,207', '67,091', '65,737', '72,903', '64,089', '61,372'],
+  'Provision for income taxes': ['-14,527', '-9,680', '-10,481', '-13,372', '-15,738', '-15,685'],
+  'Net income': ['94,680', '57,411', '55,256', '59,531', '48,351', '45,687']
+};
 
 const TextConverter = () => {
   const [file, setFile] = useState(null);
   const [isModal, setIsModal] = useState(null);
-  const handleChange = file => {
+  const handleChange = (file) => {
     // setIsModal(true);
     setloading(true);
     setTimeout(() => {
@@ -80,72 +80,79 @@ const TextConverter = () => {
       <Helmet>
         <title>Text Converter</title>
       </Helmet>
-      {(!file) ?
-        <Box marginTop="250px" marginLeft="400px" align="center">
-          {loading && <Spinner />}
-          <FileUploader
-            style={{ height: "200px" }}
-            handleChange={handleChange}
-            name="file"
-            types={fileTypes}
-          />
-        </Box> :
-        isModal ? <Dialog
-          open={isModal}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">
-            Use Google's location service?
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Let Google help apps determine location. This means sending anonymous
-              location data to Google, even when no apps are running.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Disagree</Button>
-            <Button onClick={handleClose} autoFocus>
-              Agree
-            </Button>
-          </DialogActions>
-        </Dialog> :
-          <Grid container spacing={10}>
-            <Grid item md={6}>
-              <Box
-                sx={{
-                  backgroundColor: 'background.default',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  height: '100%',
-                  justifyContent: 'center'
-                }}
-              >
-                <Container maxWidth="sm" style={{ overflow: 'auto', marginTop: '20px' }}>
-                  <Table>
-                    <TableBody>
-                      <TableHead>
-                        Apple Inc. (Consolidated Income Statement)
-                      </TableHead>
-                      {/* <TableRow>
+      {(!file)
+        ? (
+          <Box marginTop="250px" marginLeft="400px" align="center">
+            {loading && <Spinner />}
+            <FileUploader
+              style={{ height: '200px' }}
+              handleChange={handleChange}
+              name="file"
+              types={fileTypes}
+            />
+          </Box>
+        )
+        : isModal ? (
+          <Dialog
+            open={isModal}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              Use Google's location service?
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Let Google help apps determine location. This means sending anonymous
+                location data to Google, even when no apps are running.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Disagree</Button>
+              <Button onClick={handleClose} autoFocus>
+                Agree
+              </Button>
+            </DialogActions>
+          </Dialog>
+        )
+          : (
+            <Grid container spacing={6}>
+              <Grid item md={6}>
+                <Box
+                  sx={{
+                    backgroundColor: 'background.default',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '100%',
+                    justifyContent: 'center'
+                  }}
+                >
+                  <Container maxWidth="sm" style={{ overflow: 'auto', marginTop: '20px' }}>
+                    <Table>
+                      <TableBody>
+                        <TableHead>
+                          Apple Inc. (Consolidated Income Statement)
+                        </TableHead>
+                        {/* <TableRow>
                         <TableCell>
                           Consolidated Income Statement
                         </TableCell>
                       </TableRow> */}
-                      {Object.values(tableData).map((values, index) => <TableRow>
-                        <TableCell>{Object.keys(tableData)[index]}</TableCell>
-                        {values.map((value) => < TableCell>{value}</TableCell>
-                        )}
-                      </TableRow>)}
-                    </TableBody>
-                  </Table>
-                  <BasicModal />
-                </Container>
-              </Box>
+                        {Object.values(tableData).map((values, index) => (
+                          <TableRow>
+                            <TableCell>{Object.keys(tableData)[index]}</TableCell>
+                            {values.map((value) => <TableCell>{value}</TableCell>)}
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                    <BasicModal />
+                  </Container>
+                </Box>
+              </Grid>
             </Grid>
-          </Grid>}
+          )}
 
     </>
   );
