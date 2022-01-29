48f21dbdb375ec4f4fd9150379a46ca7947a1140
Anuj Bhatia <anuj@connect.hku.hk>
2022-01-29 19:48:06 +0800

    Temp Pusgh

--- react-frontend/src/components/product/ProductCard.js
+++ react-frontend/src/components/product/ProductCard.js
@@ -1,61 +1,104 @@
+import * as React from 'react';
 import PropTypes from 'prop-types';
 import {
   Avatar,
   Box,
   Card,
   CardContent,
-  Divider,
+  Button,
   Grid,
-  Typography
+  Typography,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
 } from '@material-ui/core';
 import {
   User as UserIcon,
 } from 'react-feather';
 
-const ProductCard = ({ product, ...rest }) => (
-  <Card
-    sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      height: '100%'
-    }}
-    {...rest}
-  >
-    <CardContent>
-      <Box
+export default function ProductCard({ product, ...rest }) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <Card
         sx={{
           display: 'flex',
-          justifyContent: 'center',
-          pb: 3
+          flexDirection: 'column',
+          height: '100%'
         }}
+        {...rest}
+        onClick={handleClickOpen}
       >
-        <Avatar
-          alt="Product"
-          src={product.media}
-          variant="square"
-        />
-      </Box>
-      <Typography
-        align="center"
-        color="textPrimary"
-        gutterBottom
-        variant="h4"
+        <CardContent>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              pb: 3
+            }}
+          >
+            <Avatar
+              alt="Product"
+              src={product.media}
+              variant="square"
+            />
+          </Box>
+          <Typography
+            align="center"
+            color="textPrimary"
+            gutterBottom
+            variant="h4"
+          >
+            {product.title}
+            (
+            {product.ticker}
+            )
+          </Typography>
+          <Typography
+            align="center"
+            color="textPrimary"
+            variant="body1"
+          >
+            {product.description}
+          </Typography>
+        </CardContent>
+      </Card>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
       >
-        {product.title}({product.ticker})
-      </Typography>
-      <Typography
-        align="center"
-        color="textPrimary"
-        variant="body1"
-      >
-        {product.description}
-      </Typography>
-    </CardContent>
-  </Card>
-);
+        <DialogTitle id="alert-dialog-title">
+          Use Google's location service?
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous
+            location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={handleClose} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
 
 ProductCard.propTypes = {
   product: PropTypes.object.isRequired
 };
-
-export default ProductCard;
